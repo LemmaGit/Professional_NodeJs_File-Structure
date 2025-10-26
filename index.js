461a5fae3ef660bb811d5f96f3bea3fba00eacc1
LemmaGetiye <lemmagetiye1994@gmail.com>
2025-10-26 15:51:34 +0300

    changed to http.createServer to make it suitable for working with WebSockets and also created a server file to separate creating the app and starting the server.

--- index.js
+++ index.js
@@ -2,37 +2,24 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
-const express = require("express");
-const mongoose = require("mongoose");
-const router = require("./routes/blog.route");
 const config = require("./config/config");
-const { errorHandler, errorConvertor } = require("./middlewares/error");
-const ApiError = require("./utils/ApiError");
-const { StatusCodes } = require("http-status-codes");
-
-const app = express();
+const mongoose = require("mongoose");
+const http = require("http");
+const app = require("./server");
 
 mongoose
   .connect(config.dbConnection)
   .then(() => {
-    console.log("connected to mongodb");
+    console.log("Connected to Mongodb");
   })
   .catch((err) => {
     console.error(err);
   });
 
-app.use(express.json());
-app.use(router);
-app.use((req, res, next) =>
-  next(new ApiError(StatusCodes.NOT_FOUND, "Not found"))
-);
-app.use(errorConvertor);
-app.use(errorHandler);
-
-const server = app.listen(config.port, () => {
+const httpServer = http.createServer(app);
+const server = httpServer.listen(config.port, () => {
   console.log(`Server listening on port ${config.port}`);
 });
-
 process.on("unhandledRejection", (err) => {
   if (server) {
     server.close(() => {
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = require("./routes/blog.route");
+const { errorHandler, errorConvertor } = require("./middlewares/error");
+const ApiError = require("./utils/ApiError");
+const { StatusCodes } = require("http-status-codes");
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+app.use((req, res, next) =>
+  next(new ApiError(StatusCodes.NOT_FOUND, "Not found"))
+);
+app.use(errorConvertor);
+app.use(errorHandler);
+
+module.exports = app;
